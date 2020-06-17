fcf1fdce805d4d8842d85538a0593245a4f798e3
yongniuniu <2053160852@qq.com>
2020-06-17 21:07:37 +0800

    第一节课

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void tetsgetRequest(View view) throws ExecutionException, InterruptedException {
+        System.out.println(OkHttpUtil.getReuest("https://www.baidu.com"));
     }
 }
--- app/src/main/java/com/example/myapplication/OkHttpUtil.java
+++ app/src/main/java/com/example/myapplication/OkHttpUtil.java
@@ -8,20 +8,38 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * OkHttp工具类
  *
  * */
 public class OkHttpUtil {
-   static ExecutorService threadPoll= Executors.newFixedThreadPool(30);
+   static OkHttpClient okHttpClient=new OkHttpClient();
 
+    /*创建线程池*/
+   static ExecutorService threadPoll= Executors.newFixedThreadPool(30);
     /* 发送get请求*/
-    public static String getReuest(String url) throws ExecutionException, InterruptedException {
+    public static String getReuest(final String url) throws ExecutionException, InterruptedException {
         FutureTask futureTask=new FutureTask(new Callable() {
             @Override
             public Object call() throws Exception {
                 //实现http内容
-                return null;
+                Request request=new Request.Builder()
+                        .url(url)
+                        .build();
+                /*打开网址*/
+                //得到服务器响应
+                Response response=okHttpClient.newCall(request).execute();
+                //预处理
+                if(response.isSuccessful() && response.body()!=null){
+                    return  response.body().string();
+                }else{
+                    return  null;
+                }
+
             }
         });
         threadPoll.execute(futureTask);

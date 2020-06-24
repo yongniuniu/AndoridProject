0cae644320346adb8146ba80515de853294044e3
yongniuniu <59137777+yongniuniu@users.noreply.github.com>
2020-06-24 22:15:40 +0800

    6月24日提交

---
+++ app/src/main/java/com/example/myapplication/IndexActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class IndexActivity extends FragmentActivity implements Callbacks {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+
+    @Override
+    public void onItemSelected(int id) {
+        Intent intent;
+        switch (id){
+            case 0:
+                //跳转到查看竞得物品界面
+                intent = new Intent(this,ItemActivity.class);
+                intent.putExtra("url","");
+                startActivity(intent);
+            case 1:
+                //跳转到流拍物品界面
+            case 2:
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ListFragment.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ListFragment extends Fragment {
+    Callbacks callbacks;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //Fragment加载布局文件，得到一个view组件
+        View view =  inflater.inflate(R.layout.fragment_list,container,false);
+        ListView listView = view.findViewById(R.id.listview);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                callbacks.onItemSelected(position);
+            }
+        });
+        //将view组件作为返回值返回
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        callbacks = (Callbacks) context;
+    }
+}

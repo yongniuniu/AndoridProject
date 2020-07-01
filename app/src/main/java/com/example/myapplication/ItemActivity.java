18b92937f35084df4b7cb235847eaff94925e734
yongniuniu <59137777+yongniuniu@users.noreply.github.com>
2020-07-01 21:20:16 +0800

    七月一日提交

--- app/src/main/java/com/example/myapplication/ItemActivity.java
+++ app/src/main/java/com/example/myapplication/ItemActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ItemActivity extends AppCompatActivity {
+import com.example.myapplication.dummy.DummyContent;
+
+public class ItemActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayout,ItemFragment
+                        .newInstance(1)).commit();
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,48 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-
+   private  EditText editText1;
+   private EditText editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText1=findViewById(R.id.editText1);
+        editText2=findViewById(R.id.editText2);
+        initView();
     }
     public void tetsgetRequest(View view) throws ExecutionException, InterruptedException {
         System.out.println(OkHttpUtil.getReuest("https://www.baidu.com"));
     }
+    public void tetsgetPost(View view) throws ExecutionException, InterruptedException {
+      Map<String,String> map=new HashMap<String ,String>(){{
+          put("username",editText1.getText().toString());
+          put("password",editText2.getText().toString());
+      }};
+        String result=OkHttpUtil.postReuest("http://172.18.85.254:8080/auction/api/users/login",map);
+        if (result!=null && result.equals("1")){
+            Intent intent=new Intent(this,IndexActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this,"登录失败",Toast.LENGTH_LONG).show();
+        }
+    }
+    private void initView(){
+        editText1=findViewById(R.id.editText1);
+        editText2=findViewById(R.id.editText2);
+    }
+
+
 }
